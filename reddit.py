072a7016cfc5fdbb9b48a05fdbdd198efae5cc65
berk <sbgonenc@gmail.com>
2024-06-16 16:49:03 +0200

    under development - implemented subreddit search, exact and fuzzy query searches

--- reddit.py
+++ reddit.py
@@ -1,74 +1,108 @@
 from settings import CONFIG_FILE_PATH
 import re
-from praw.models import MoreComments
+from praw.models import MoreComments, Subreddit, ListingGenerator
+from typing import Iterator, Any, AnyStr, Union, Dict, List
 
 
-class Reddit:
-    def __init__(self, thread_limit=1, include_nsfw:bool=False, subreddits=None):
-        self.config_file_path = CONFIG_FILE_PATH
+class RedditContent:
+    """
+    Authenticates to Reddit,
+    Search for subreddits, get threads and comments.
+
+    """
+    def __init__(self, thread_limit=20, include_nsfw:bool=False, config_file_path:str=None):
+        self.config_file_path = CONFIG_FILE_PATH if config_file_path is None else config_file_path
         self.thread_limit = thread_limit
         self.include_nsfw = include_nsfw
 
         self.reddit = None
-        self.subreddits = subreddits
         self.contents = {}
 
-    def process_popular(self):
+    def process(self,
+                search_query:str=None,
+                exact_search:bool=False,
+                fuzzy_search:bool=False,
+                subreddits:List[AnyStr]=None
+                ):
         ## Authenticate if not already authenticated
         if not self.reddit:
             self.authenticate()
 
-        ## Get popular subreddits
-        self.subreddits = self.get_popular_subreddits(),
+        ## Get query related subreddits if search query is provided
+        ## default is to have popular subreddits
+        if search_query is None and subreddits is None:
+            subs = self.get_popular_subreddits()
 
-        ## Get threads from popular subreddits
-        subnames= []
-        for sub in self.subreddits:
-            self.contents["subreddit"] = {sub.title: {
-                "title": sub.title,
-                #"description": sub.description,
-                "display_name": sub.display_name,
-                "contents" : []
-            }}
-            subnames.append(sub.display_name)
+        elif search_query is not None:
+            if fuzzy_search: subs = self.search_subreddits(search_query)
+            else: subs = self.search_subreddits_by_name(search_query, exact=exact_search)
+            #self.update_content_subreddits(subs)
+        elif subreddits is not None:
+            for subred in subreddits:
+                self.process(search_query=subred, exact_search=True)
+            return 0
+
+        else:
+            raise Exception("Invalid arguments provided. Please provide either search_query or subreddits.")
+
+        ## Get threads from subreddits
+        self.update_content_subreddits(subs)
 
         ##get threads and comments
-        for thread in self.get_subreddit_threads(subnames):
-            self.contents["subreddit"][thread.title]["contents"].append({
-                "title": thread.title,
-                "comments": self.get_comments(thread.id)
-            })
-
+        self.update_content_threads_comments()
 
     def search_subreddits(self, query:str):
         """
-        Search for subreddits' title and description by query
+        Search for subreddits title and description by query
         :param query:
-        :param exact:
         :return:
         """
-        return self.reddit.subreddits.search(query=query)
+        return self.reddit.subreddits.search(query=query, include_nsfw=self.include_nsfw)
 
     def search_subreddits_by_name(self, query:str, exact:bool=False):
         return self.reddit.subreddits.search_by_name(query, exact=exact, include_nsfw=self.include_nsfw)
 
-    def get_subreddit_threads(self, subreddits:list):
-        subreddit = self.reddit.subreddit("+".join(subreddits))
-        thread_objs = subreddit.hot(limit=self.thread_limit)
-        return thread_objs
+    def get_subreddits(self, subreddits:list|str)->Subreddit:
+        if isinstance(subreddits, str):
+            return self.reddit.subreddit(subreddits)
+        return self.reddit.subreddit("+".join(subreddits))
+
+    def get_subreddit_threads(self, subreddits:List[AnyStr])->Iterator[Any]:## subreddit object of the subreddits
+        return self.get_subreddits(subreddits).hot(limit=self.thread_limit)
 
     def get_popular_subreddits(self):
         return self.reddit.subreddits.popular()
 
-    @staticmethod
-    def get_post_id(reddit_obj):
-        """"
-        This function takes a reddit object and returns the post id
-        """
-        return reddit_obj.id
-        #return re.sub(r"[^\w\s-]", "", reddit_obj["thread_id"])
+    def update_content_subreddits(self, subreddits:List[Subreddit]):
+        for sub in subreddits:
+            r_url = sub.url.split("/")[2]
+            self.contents.update({r_url: {
+                "title": sub.title,
+                "description": sub.description,
+                "display_name": sub.display_name,
+                "url": r_url,
+                "contents": []
+            }})
 
-    def get_comments(self, post_id, sort_by="top", max_comment_length=2000, min_comment_length=10):
+    def update_content_threads_comments(self):
+        ##get display names for the subreddits
+        subreddit_names = [sub["display_name"] for sub in self.contents.values()]
+        threads_comments = self.get_subreddit_threads(subreddit_names)
+        for idx, thread in enumerate(threads_comments):
+            comments = self.get_comments(thread.id)
+            if not comments: continue
+            r_url = comments[0]["url"].split("/")[2]
+            self.contents[r_url]["contents"].append({
+                "thread_id": thread.id,
+                "title": thread.title,
+                "comments": comments,
+                "is_nsfw": thread.over_18,
+                "upvotes": thread.score,
+                "thread_url": thread.url,
+                "upvote_ratio": thread.upvote_ratio,
+            })
+
+    def get_comments(self, post_id, sort_by="top", max_comment_length=2000, min_comment_length=100):
         submission = self.reddit.submission(post_id)
         submission.comment_sort = sort_by
         comment_list = submission.comments.list()
@@ -88,7 +122,7 @@ class Reddit:
             sanitised_text = self.sanitise_text(top_level_comment.body)
             if sanitised_text is None or sanitised_text.isspace(): continue
             comments.append({
-                "body": sanitised_text,
+                "text": sanitised_text,
                 "id": top_level_comment.id,
                 "url": top_level_comment.permalink,
                 "author": top_level_comment.author.name
@@ -129,25 +163,7 @@ class Reddit:
         # remove extra whitespace
         return " ".join(text.split())
 
-
-rdt = Reddit()
-rdt.authenticate()
-subreddits = []
-for sub in rdt.get_popular_subreddits():
-    #print(sub.title, sub.display_name)
-    subreddits.append(sub.display_name)
-
-threads = rdt.get_subreddit_threads(subreddits)
-for thread in threads:
-    print("Thread Title:\t" + thread.title)
-    print("Thread URL:\t" + thread.url)
-    print("Thread ID:\t" + thread.id)
-    print("Thread Upvotes:\t" + str(thread.score))
-    print("Thread Upvote Ratio:\t" + str(thread.upvote_ratio))
-    print("Thread Comments:\t" + str(thread.num_comments))
-    print("Thread NSFW:\t" + str(thread.over_18))
-    print("Thread Self Text:\t" + thread.selftext)
-    print("Thread Author:\t" + thread.author.name)
-
-
-    #print(rdt.get_comments(thread.id))
+    def write_contents(self, out_file_path:str):
+        import json
+        with open(out_file_path, "w") as f:
+            json.dump(self.contents, f, indent=4)
